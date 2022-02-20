dc788ad8ea103fb823118809d2a2f155bac95c98
Perry Guloien <perryaguloien@gmail.com>
2022-02-20 16:23:01 -0500

    updating sendgrid receipt

--- shopping_cart.py
+++ shopping_cart.py
@@ -10,42 +10,6 @@ from sendgrid.helpers.mail import Mail
 
 load_dotenv()
 
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
-SENDGRID_TEMPLATE_ID = os.getenv("SENDGRID_TEMPLATE_ID", default="OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
-SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
-
-# this must match the test data structure
-template_data = {
-    "total_price_usd": "$14.95",
-    "human_friendly_timestamp": "June 1st, 2019 10:00 AM",
-    "products":[
-        {"id":1, "name": "Product 1"},
-        {"id":2, "name": "Product 2"},
-        {"id":3, "name": "Product 3"},
-        {"id":2, "name": "Product 2"},
-        {"id":1, "name": "Product 1"}
-    ]
-} # or construct this dictionary dynamically based on the results of some other process :-D
-
-client = SendGridAPIClient(SENDGRID_API_KEY)
-print("CLIENT:", type(client))
-
-message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS)
-message.template_id = SENDGRID_TEMPLATE_ID
-message.dynamic_template_data = template_data
-print("MESSAGE:", type(message))
-
-try:
-    response = client.send(message)
-    print("RESPONSE:", type(response))
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-
-except Exception as err:
-    print(type(err))
-    print(err)
-
 
 # ... where they can be accessed / read via the os module as usual:
 tax = float(os.getenv("TAX_RATE"))
@@ -156,31 +120,68 @@ print("TOTAL: "+(str(to_usd(total))))
 print("---------------------------------")
 print("THANKS, SEE YOU AGAIN SOON!")
 print("---------------------------------")
+print(matching_products)
+print(matching_product)
+email = input("Would the customer like an email receipt as well? Please input 'y' or 'n'.")
 
+if email=="n":
+    quit
+else:
+    SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
+    SENDGRID_TEMPLATE_ID = os.getenv("SENDGRID_TEMPLATE_ID", default="OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
+    SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
 
+    # this must match the test data structure
+    template_data = {
+        "subtotal_price_usd": str(to_usd(total_price)),
+        "tax_usd": str(to_usd(tax_value)),
+        "total_price_usd": str(to_usd(total)),
+        "date": now.strftime("%m/%d/%Y"),
+        "time":now.strftime("%H:%M:%S"),
+        "products": "hello"
+    
+    } 
+    # or construct this dictionary dynamically based on the results of some other process :-D
 
-#print(product_id) #> "9"
-#print(type(product_id)) #> str
+    client = SendGridAPIClient(SENDGRID_API_KEY)
+    #print("CLIENT:", type(client))
 
-# LOOK UP CORRESPONDING PRODUCTS
+    message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS)
+    message.template_id = SENDGRID_TEMPLATE_ID
+    message.dynamic_template_data = template_data
+    #print("MESSAGE:", type(message))
 
-# print product that has an id attribute equal to "9"
+    try:
+       response = client.send(message)
+       # print("RESPONSE:", type(response))
+       # print(response.status_code)
+      #  print(response.body)
+       # print(response.headers)
 
-# matching_products = []
+    except Exception as err:
+       # print(type(err))
+       # print(err)
 
-#for x in products:
-    #if x == 3:
-    #    ___.append(x)
-    #print(x)
-    #print(x["id"])
-   # if str(x["id"]) == str(product_id):
-        # this is a match
-        # matching_products.append(x)
+    #print(product_id) #> "9"
+    #print(type(product_id)) #> str
 
-#print(matching_products["name"])
+    # LOOK UP CORRESPONDING PRODUCTS
 
-#print the name of the matching product: 
-#print(len(matching_products))
-#print(type(matching_products))
+    # print product that has an id attribute equal to "9"
 
-#LOOK UP CORRESPONDING PRODUCTS
+    # matching_products = []
+
+    #for x in products:
+        #if x == 3:
+        #    ___.append(x)
+        #print(x)
+        #print(x["id"])
+    # if str(x["id"]) == str(product_id):
+            # this is a match
+            # matching_products.append(x)
+
+    #print(matching_products["name"])
+
+    #print the name of the matching product: 
+    #print(len(matching_products))
+    #print(type(matching_products))
